2aa5abca872fc706a3ce50b9f0dd62870e471fc9
mustafa badan <62083173+mustafabadan1@users.noreply.github.com>
2023-04-09 23:37:40 +0300

    i dont fckn know what happen here!

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { HiOutlineMenu } from 'react-icons/hi';
 import { Navdata } from '@/constant/Data';
-import NavDesktop from './NavDesktop';
-import NavMobile from './NavMobile';
+import Link from 'next/link';
+
+type linkProps = {
+  title?: string;
+  reference: string;
+};
 
 const Nav = () => {
   const [toggle, setToggle] = useState(true);
@@ -17,27 +21,53 @@ const Nav = () => {
           <HiOutlineMenu color={'#f3f4f6'} size={30} />
         </button>
       </div>
-      {/* mobile */}
+
+      {/* //=mobile */}
       {!toggle ? (
         <nav
           id='nav-menu'
           className={`lg:hidden absolute top-[4.5rem] right-4 py-[0.6rem] px-3 bg-gray-800`}
         >
           <ul className='flex flex-col gap-2 pr-2 text-gray-800'>
+            {/* mapping title,link */}
             {Navdata.map((item) => (
               <div key={item.reference}>
-                <NavMobile title={item.title} reference={item.reference} />
+                <li className='group'>
+                  {/* nav link */}
+
+                  <Link
+                    href={`#${item.reference}`}
+                    className={`px-2 text-[.95rem] hover:text-opacity-60 text-gray-100`}
+                  >
+                    {/* nav title */}
+                    {item.title}
+                  </Link>
+                </li>
               </div>
             ))}
           </ul>
         </nav>
       ) : null}
+
       {/* desktop */}
       <nav id='nav-menu' className='hidden lg:block'>
-        <ul className='flex text-gray-800'>
+        <ul className='flex'>
+          {/* mapping title,link */}
           {Navdata.map((item) => (
             <div key={item.reference}>
-              <NavDesktop title={item.title} reference={item.reference} />
+              <li className='group'>
+                {/* nav link */}
+
+                <Link
+                  href={`#${item.reference}`}
+                  className='px-3 mx-2 py-1 xl:mx-3 hover:text-opacity-80 
+                  text-gray-100'
+                >
+                  {/* nav title */}
+
+                  {item.title}
+                </Link>
+              </li>
             </div>
           ))}
         </ul>
